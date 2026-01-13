3f4e704500442e4de1ecf1bda14677ece2f3bede
Tomasz Kumor <frgit@MacBook-Air-FRG.home>
2026-01-13 10:58:58 +0100

    basePageFactory - part 1

---
+++ src/main/java/basePageFactory/BasePageFactory.java
@@ -0,0 +1,43 @@
+package basePageFactory;
+
+import actions.*;
+import config.TestStackProperties;
+import io.appium.java_client.pagefactory.AppiumFieldDecorator;
+import org.openqa.selenium.support.PageFactory;
+
+import static driver.BaseDriver.getWebDriver;
+
+public class BasePageFactory {
+    public CheckActions check;
+    public ClickActions click;
+    public GetActions get;
+    public SendActions send;
+    public BrowserActions browser;
+    public MobileActions mobile;
+
+    protected BasePageFactory() {
+        this.check = new CheckActions();
+        this.click = new ClickActions();
+        this.get = new GetActions();
+        this.send = new SendActions();
+        this.browser = new BrowserActions();
+        this.mobile = new MobileActions();
+        initElements();
+    }
+
+    private void initElements() {
+        String framework = TestStackProperties.getFramework();
+        switch (framework) {
+            case "selenium", "api" -> initElementsForDesktopTest();
+            case "appium" -> initElementsForMobileTest();
+        }
+    }
+
+    private void initElementsForMobileTest() {
+        PageFactory.initElements(new AppiumFieldDecorator(getWebDriver().getDriver()), this);
+    }
+
+    private void initElementsForDesktopTest() {
+        PageFactory.initElements(getWebDriver().getDriver(), this);
+    }
+}
